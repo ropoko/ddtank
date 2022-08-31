3ecb7c053e076982ba70432d82247b19ff75bf03
ropoko <rodrigostramantinoli@gmail.com>
2022-08-31 18:13:19 -0300

    refactor: dash function

--- main.lua
+++ main.lua
@@ -11,38 +11,48 @@ local ENEMY = {
 	size = 15
 }
 
+local function dash(direction, signal)
+	if love.keyboard.isDown('space') then
+		if signal == '-' then
+			PLAYER[direction] = PLAYER[direction] - 3
+		else
+			PLAYER[direction] = PLAYER[direction] + 3
+		end
+	end
+end
+
 local function move_player()
+	local last_movement = {}
+
 	if love.keyboard.isDown('w') then
 		PLAYER.y = PLAYER.y - 1
 
-		if love.keyboard.isDown('space') then
-			PLAYER.y = PLAYER.y - 3
-		end
+		last_movement.direction = 'y'
+		last_movement.signal = '-'
 	end
 
 	if love.keyboard.isDown('s') then
 		PLAYER.y = PLAYER.y + 1
 
-		if love.keyboard.isDown('space') then
-			PLAYER.y = PLAYER.y + 3
-		end
+		last_movement.direction = 'y'
+		last_movement.signal = '+'
 	end
 
 	if love.keyboard.isDown('a') then
 		PLAYER.x = PLAYER.x - 1
 
-		if love.keyboard.isDown('space') then
-			PLAYER.x = PLAYER.x - 3
-		end
+		last_movement.direction = 'x'
+		last_movement.signal = '-'
 	end
 
 	if love.keyboard.isDown('d') then
 		PLAYER.x = PLAYER.x + 1
 
-		if love.keyboard.isDown('space') then
-			PLAYER.x = PLAYER.x + 3
-		end
+		last_movement.direction = 'x'
+		last_movement.signal = '+'
 	end
+
+	dash(last_movement.direction, last_movement.signal)
 end
 
 function love.update()
