99bcd912623cf2d0c6c0a4734cbf0dd747489902
ropoko <rodrigostramantinoli@gmail.com>
2022-12-26 23:39:18 -0300

    feat: wip aim

--- player.lua
+++ player.lua
@@ -7,17 +7,7 @@ local Player = {
 
 local center_user = {}
 
-local selected_angle = 0
-
-function love.keypressed(key)
-	if key == 'w' then
-		selected_angle = selected_angle + 1
-	end
-
-	if key == 's' then
-		selected_angle = selected_angle - 1
-	end
-end
+local angle = 0
 
 function Player:load()
 	self.body = love.physics.newBody(WORLD, self.x, self.y, 'dynamic')
@@ -26,6 +16,14 @@ function Player:load()
 end
 
 function Player:update(dt)
+	if love.keyboard.isDown('w') then
+		angle = angle + math.pi * dt * 0.2
+	end
+
+	if love.keyboard.isDown('s') then
+		angle = angle - math.pi * dt * 0.2
+	end
+
 	center_user = {
 		x = Player.x + Player.width / 2,
 		y = Player.y + Player.height / 2
@@ -37,7 +35,7 @@ function Player:draw()
 	self.x, self.y,_,_ = self.body:getWorldPoints(self.shape:getPoints())
 
 	love.graphics.setColor(1,0,0)
-	love.graphics.polygon('line', self.body:getWorldPoints(self.shape:getPoints()))
+	-- love.graphics.polygon('line', self.body:getWorldPoints(self.shape:getPoints()))
 
 	self:draw_aim()
 end
@@ -47,23 +45,25 @@ local function stencilFunction()
 end
 
 function Player:draw_aim()
-	love.graphics.print(tostring(selected_angle), 10, 10)
-
-	local aim_x = center_user.x + math.cos(selected_angle)*100
-	local aim_y = center_user.y + math.sin(selected_angle)*100
-
-	-- love.graphics.circle('fill', aim_x, aim_y, 5,5)
-
-	love.graphics.setColor(1,0,0)
-	love.graphics.setLineWidth(2)
-	love.graphics.line(aim_x, aim_y, aim_x - 25, aim_y - 25)
-	love.graphics.setLineWidth(1)
+	love.graphics.print(tostring(angle), 10, 10)
 
 	love.graphics.stencil(stencilFunction, 'replace', 1)
 	love.graphics.setStencilTest('equal', 0)
 	love.graphics.setColor(228/255,228/255,228/255, 0.5)
 	love.graphics.circle('fill', center_user.x, center_user.y, 100)
 	love.graphics.setStencilTest()
+
+	love.graphics.push()
+
+	love.graphics.translate(center_user.x, center_user.y)
+	love.graphics.rotate(angle)
+	love.graphics.translate(-center_user.x, -center_user.y)
+
+	love.graphics.setColor(1,0,0)
+	love.graphics.line(center_user.x,center_user.y, center_user.x,center_user.y - 20)
+	love.graphics.setColor(1,1,1)
+
+	love.graphics.pop()
 end
 
 return Player
